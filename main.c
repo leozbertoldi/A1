2d95e53a77a7ce1bd51de639b8aa921b85a53491
Leonardo Zandonai Bertoldi <leozbertoldi@gmail.com>
2025-04-15 18:43:56 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+  int option;
+  for (int i = 1; i < argc; i++)
+  {
+    if (argv[i][0] == '-')
+      option = 1;
+    else 
+      option = 0;
+
+    if (option)
+    {
+      switch (argv[i][1])
+      {
+        case 'i':
+          if(argv[i][2] == 'p')
+            printf("caso -ip\n");
+          else if (argv[i][2] == 'c')
+            printf("caso -ic\n");
+          break;
+
+        case 'm':
+          printf("caso -m membro\n");
+          break;
+
+        case 'x':
+          printf("caso -x\n");
+          break;
+
+        case 'r':
+          printf("caso -r\n");
+          break;
+
+        case 'c':
+          printf("caso -c\n");
+          break;
+
+        default:
+          break;
+      }
+    }
+    else 
+      printf("Arquivo %s\n", argv[i]);
+  }
+
+  return 0;
+}
+
+//ftrunc 
+//if tamanho >= compactado 
+//if compactado > normal armazena normal (se der o mesmo tamanhop, ficar com o plano)
+//nota extra pra permissões do arquivo
